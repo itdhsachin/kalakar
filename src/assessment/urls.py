58cbba6ef4a0cf45035c3a2eae7147f7c5bb5d22
avishkar-sapkal <sapkalaviskar@gmail.com>
2025-02-11 12:47:18 +0530

    Urls files

--- src/assessment/urls.py
+++ src/assessment/urls.py
@@ -1,19 +1,41 @@
 from django.urls import path
-from . import views
+
+from assessment import views
 
 urlpatterns = [
-    path('upload/', views.upload, name='upload'),
-    path('thank-you/', views.thankyou, name='thankyou'),
-
+    path("upload/", views.upload, name="upload"),
+    path("thank-you/", views.thankyou, name="thankyou"),
+    path("login-redirect/", views.login_redirect, name="login_redirect"),
     # Student Competition
-    path('competitions/', views.student_competition_list, name='student_competition_list'),
-    path('competitions/add/', views.student_competition_add, name='student_competition_add'),
-    path('competitions/edit/<int:competition_id>/', views.student_competition_edit, name='student_competition_edit'),
-    path('competitions/delete/<int:competition_id>/', views.student_competition_delete, name='student_competition_delete'),
-
+    path(
+        "competitions/",
+        views.student_competition_list,
+        name="student_competition_list",
+    ),
+    path(
+        "competitions/add/",
+        views.student_competition_add,
+        name="student_competition_add",
+    ),
+    path(
+        "competitions/edit/<int:competition_id>/",
+        views.student_competition_edit,
+        name="student_competition_edit",
+    ),
+    path(
+        "competitions/delete/<int:competition_id>/",
+        views.student_competition_delete,
+        name="student_competition_delete",
+    ),
     # Work Submission
-    path('assigned-works/', views.assigned_work_list, name='assigned_work_list'),
-    path('submit-review/<int:work_id>/', views.submit_review, name='submit_review'),
-    path('work/edit/<int:work_id>/', views.edit_work, name='edit_work'),
-    path('work/delete/<int:work_id>/', views.delete_work, name='delete_work'),
+    path(
+        "assigned-works/", views.assigned_work_list, name="assigned_work_list"
+    ),
+    path(
+        "submit-review/<int:work_id>/",
+        views.submit_review,
+        name="submit_review",
+    ),
+    path("work/edit/<int:work_id>/", views.edit_work, name="edit_work"),
+    path("work/delete/<int:work_id>/", views.delete_work, name="delete_work"),
 ]
