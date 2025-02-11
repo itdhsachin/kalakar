0f3e08279be8ac4225b5406b540b2aa327f0d3c4
avishkar-sapkal <sapkalaviskar@gmail.com>
2025-02-11 12:42:18 +0530

    Forms File

--- src/assessment/forms.py
+++ src/assessment/forms.py
@@ -1,15 +1,32 @@
 from django import forms
-from .models import StudentCompetition, AssessmentUpload
+
+from assessment.models import AssessmentUpload, StudentCompetition
+
 
 class StudentCompetitionForm(forms.ModelForm):
-    class Meta:
-        model = StudentCompetition
-        fields = ['user', 'status']
+    """Form for managing student competition entries."""
 
-    status = forms.ChoiceField(choices=[(1, 'Active'), (0, 'Inactive')], widget=forms.RadioSelect)
+    class Meta:
+        """Metadata for StudentCompetitionForm."""
+
+        model = StudentCompetition
+        fields = ["user", "status"]
+
+    status = forms.ChoiceField(
+        choices=[(1, "Active"), (0, "Inactive")], widget=forms.RadioSelect
+    )
+
 
 class ReviewForm(forms.ModelForm):
+    """Form for submitting reviews for assessment uploads."""
+
     class Meta:
+        """Metadata for StudentCompetitionForm."""
+
         model = AssessmentUpload
-        fields = ['review_score']
-        widgets = {'review_score': forms.RadioSelect(choices=[(i, str(i)) for i in range(1, 6)])}
+        fields = ["review_score"]
+        widgets = {
+            "review_score": forms.RadioSelect(
+                choices=[(i, str(i)) for i in range(1, 6)]
+            )
+        }
