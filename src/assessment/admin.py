7db48456108ae97e1cb60e844f4be68e45f9bfdf
avishkar-sapkal <sapkalaviskar@gmail.com>
2025-02-11 12:41:20 +0530

    Admin File

--- src/assessment/admin.py
+++ src/assessment/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
-from .models import StudentCompetition, AssessmentUpload
+
+from assessment.models import AssessmentUpload, StudentCompetition
+
 
 @admin.register(StudentCompetition)
 class StudentCompetitionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'status', 'created_date')
-    list_filter = ('status',)
-    search_fields = ('user__username',)
+    """Admin interface for managing student competitions."""
+
+    list_display = ("user", "status", "created_date")
+    list_filter = ("status",)
+    search_fields = ("user__username",)
+
 
 @admin.register(AssessmentUpload)
 class AssessmentUploadAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'filename', 'assigned_teacher', 'review_score', 'timestamp')
-    list_filter = ('assigned_teacher', 'review_score')
-    search_fields = ('user_id__username', 'assigned_teacher__username')
-    ordering = ('-timestamp',)
-    list_editable = ('assigned_teacher',)  # Allow admin to assign a teacher
+    """Admin interface for managing assessment uploads."""
+
+    list_display = (
+        "user_id",
+        "filename",
+        "assigned_teacher",
+        "review_score",
+        "timestamp",
+    )
+    list_filter = ("assigned_teacher", "review_score")
+    search_fields = ("user_id__username", "assigned_teacher__username")
+    ordering = ("-timestamp",)
+    list_editable = ("assigned_teacher",)  # Allow admin to assign a teacher
