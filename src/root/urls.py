0b0d471071a19cbf770a7ea02068bd8ba643378c
vishalhandifod <vishalhandifod123@gmail.com>
2025-02-26 13:32:12 +0530

    accounts and payment app url

--- src/root/urls.py
+++ src/root/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
@@ -25,8 +27,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("students/", include("students.urls")),
+    path("payment/", include("payment.urls")),
 ]
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
